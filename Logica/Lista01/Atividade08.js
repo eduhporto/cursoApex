156f978166aefd7924254798d38d1aac88d6e207
eduhporto <eduardoc.porto@outlook.com>
2021-04-28 00:17:11 -0300

    Enviando atividades em andamento (commit02)

--- Logica/Lista01/Atividade08.js
+++ Logica/Lista01/Atividade08.js
@@ -2,5 +2,21 @@
 Peça três números, retorne o maior dos três
 */
 
+function atividade08() {
+    alert("Vamos fazer uma mágica de computador. Por favor, me informe três números.")
+    var num1 = prompt("Vamos lá, me informe o primeiro número:")
+    var num2 = prompt("Agora o segundo:")
+    var num3 = prompt("E por fim, o terceiro número:")
+    
+    if (parseInt(num1) > parseInt(num2) && parseInt(num3)) {
+        document.getElementById("paragrafo08").innerText = "O maior número dos três é o " + num1
+    }else if (parseInt(num2) > parseInt(num1) && parseInt(num3)){
+        document.getElementById("paragrafo08").innerText = "O maior número dos três é o " + num2
+    }else if (parseInt(num3) > parseInt(num1) && parseInt(num2)){
+        document.getElementById("paragrafo08").innerText = "O maior número dos três é o " + num3
+    }
+
+    
+}
 
 
--- Logica/Lista01/Atividade11.js
+++ Logica/Lista01/Atividade11.js
@@ -2,5 +2,20 @@
 O usuário informa um número, retorne se é um número positivo ou negativo.
 */
 
+function atividade11() {
+    var num = prompt("Informe um número qualquer:")
 
+    if (num > 0) {
+        document.getElementById("paragrafo11").innerText = "Este número é positivo"
+        
+    }else if (num < 0){
+        document.getElementById("paragrafo11").innerText = "Este número é negativo"
+    }else if (num == 0){
+        document.getElementById("paragrafo11").innerText = "Este número é neutro"
+    }
+    
+    
+    
+
+}
 
